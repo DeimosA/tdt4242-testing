22d4309dc70ce8f60ac6a0004515175e6a2c0238
Erling Ihlen <erling_pondus@hotmail.com>
2018-04-13 11:28:11 +0200

    unit tests for Software model

---
+++ SoCam/src/no/ntnu/test/unit/TestSoftware.java
@@ -0,0 +1,30 @@
+package no.ntnu.test.unit;
+
+import junit.framework.TestCase;
+import no.ntnu.fp.model.Software;
+
+public class TestSoftware extends TestCase{
+    public void testConstructorOne() {
+        Software software = new Software();
+        assertEquals(0, software.getSwVersion());
+        assertEquals(0, software.getMinorVersion());
+        assertEquals("", software.getUrl());
+    }
+
+    public void testConstructorTwo() {
+        Software software = new Software(1, 2, "someurl");
+        assertEquals(1, software.getSwVersion());
+        assertEquals(2, software.getMinorVersion());
+        assertEquals("someurl", software.getUrl());
+    }
+
+    public void testSetters() {
+        Software software = new Software();
+        software.setSwVersion(1);
+        software.setMinorVersion(2);
+        software.setUrl("someurl");
+        assertEquals(1, software.getSwVersion());
+        assertEquals(2, software.getMinorVersion());
+        assertEquals("someurl", software.getUrl());
+    }
+}
