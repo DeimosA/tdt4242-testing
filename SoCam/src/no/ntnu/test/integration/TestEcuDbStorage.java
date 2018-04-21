c5663c0721421547d3478bdc1eeae2cc5a6ef32d
DeimosA <krvolden@gmail.com>
2018-04-21 20:52:25 +0200

    Hacky predecessor test

--- SoCam/src/no/ntnu/test/integration/TestEcuDbStorage.java
+++ SoCam/src/no/ntnu/test/integration/TestEcuDbStorage.java
@@ -1,13 +1,15 @@
 package no.ntnu.test.integration;
 
 import junit.framework.TestCase;
+import no.ntnu.fp.gui.EcuPanel;
 import no.ntnu.fp.gui.FactoryProjectPanel;
-import no.ntnu.fp.model.Ecu;
-import no.ntnu.fp.model.Project;
-import no.ntnu.fp.model.SimpleEcu;
-import no.ntnu.fp.model.Software;
+import no.ntnu.fp.model.*;
+import no.ntnu.fp.storage.CreateFactoryDB;
 import no.ntnu.fp.storage.EcuDbStorage;
+import no.ntnu.fp.storage.InsertTestDataFactoryDb;
 
+import javax.swing.*;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class TestEcuDbStorage extends TestCase {
@@ -15,8 +17,10 @@ public class TestEcuDbStorage extends TestCase {
     public EcuDbStorage ecuDbStorage;
 
     public void setUp() {
+        // Reset DB first
+        CreateFactoryDB.main(new String[0]); // This will log SQLException, but don't worry
+        InsertTestDataFactoryDb.main(new String[0]);
         ecuDbStorage = new EcuDbStorage();
-        FactoryProjectPanel.main(new String[] {"123"});
     }
 
     public void tearDown() {
@@ -36,17 +40,54 @@ public class TestEcuDbStorage extends TestCase {
         assertTrue(ecuDbStorage.isUpdated(secondEcu));
     }
 
-    /*
-    // Requires panel
     public void testAddEcu() {
+        SimpleEcu simpleEcu = new SimpleEcu(5, 5);
+        ecuDbStorage.addEcu(simpleEcu, new EcuPanel(new FactoryProjectPanel()));
 
+        ArrayList<SimpleEcu> sEcuList = ecuDbStorage.openSimpleEcu();
+        boolean sEcuFound = false;
+        for (SimpleEcu sEcu : sEcuList) {
+            if (simpleEcu.getEcuId() == sEcu.getEcuId() && simpleEcu.getSwId() == sEcu.getSwId()) {
+                sEcuFound = true;
+            }
+        }
+        assertTrue(sEcuFound);
     }
-    */
 
     public void testGetEcuSoft() {
         Ecu ecu = new Ecu(1, 1, 1);
         int[] result = ecuDbStorage.getEcuSoft(ecu);
         assertTrue(result.length > 0);
+    }
+
+    public void testThroughEcuPanel() {
+        FactoryProjectPanel fpPanel = new FactoryProjectPanel();
+        fpPanel.getNewVehiclePanel().getNwpecuPanel().getNvPanel().setModel(new Vehicle());
+        FactoryProject fpModel = new FactoryProject();
+        fpPanel.setModel(fpModel);
+        EcuPanel ecuPanel = new EcuPanel(fpPanel);
+        SimpleEcu simpleEcu = new SimpleEcu(4, 4);
+        fpModel.addEcu(simpleEcu);
+        ecuPanel.setModel(simpleEcu);
+        ecuPanel.setEditable(true);
+
+        try {
+            Field saveButtonField = EcuPanel.class.getDeclaredField("saveBtn");
+            saveButtonField.setAccessible(true);
+            JButton saveButton = (JButton) saveButtonField.get(ecuPanel);
+            saveButton.doClick();
+            ArrayList<SimpleEcu> sEcuList = ecuDbStorage.openSimpleEcu();
+            boolean sEcuFound = false;
+            for (SimpleEcu sEcu : sEcuList) {
+                if (simpleEcu.getEcuId() == sEcu.getEcuId() && simpleEcu.getSwId() == sEcu.getSwId()) {
+                    sEcuFound = true;
+                }
+            }
+            assertTrue(sEcuFound);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            fail();
+        }
     }
 
 }
